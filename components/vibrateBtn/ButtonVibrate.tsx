8d9c2ad745da201ad3ea91e2410c82131e09585c
Javasrabni <javasrabnii@gmail.com>
2025-12-07 13:22:23 +0700

    UP

--- components/vibrateBtn/ButtonVibrate.tsx
+++ components/vibrateBtn/ButtonVibrate.tsx
@@ -3,7 +3,7 @@ import { Haptics, ImpactStyle } from '@capacitor/haptics';
 
 const vibrate = async () => {
   await Haptics.impact({
-    style: ImpactStyle.Medium,
+    style: ImpactStyle.Heavy,
   });
 };
 
